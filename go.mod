1d35b11876752726b47c26405af08bb6243c742a
ngergs <niklas@ngergs.de>
2023-01-15 13:09:06 +0100

    feat: better scaling with kube-builder
    BREAKING CHANGE: health and metrics related flags have changed
    
    We now use kube-builder to support more granular updates of the ingress states.
    We also use the health, ready and metrics server from kubebuilder.
    The custom metrics introduced before are not altered.

--- go.mod
+++ go.mod
@@ -6,13 +6,13 @@ replace github.com/ngergs/ingress => ./
 
 require (
 	github.com/lucas-clemente/quic-go v0.31.1
-	github.com/ngergs/websrv v1.9.8
-	github.com/prometheus/client_golang v1.14.0
+	github.com/ngergs/websrv v1.10.0
 	github.com/rs/zerolog v1.28.0
 	github.com/stretchr/testify v1.8.1
 	k8s.io/api v0.26.0
 	k8s.io/apimachinery v0.26.0
 	k8s.io/client-go v0.26.0
+	sigs.k8s.io/controller-runtime v0.14.1
 )
 
 require (
@@ -21,18 +21,22 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.10.1 // indirect
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
-	github.com/go-openapi/jsonreference v0.20.1 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20230111200839-76d1ae5aea2b // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -49,6 +53,7 @@ require (
 	github.com/onsi/ginkgo/v2 v2.7.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.39.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
@@ -64,11 +69,14 @@ require (
 	golang.org/x/text v0.6.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.5.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.26.0 // indirect
+	k8s.io/component-base v0.26.0 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230109183929-3758b55a6596 // indirect
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
--- state/ingress.go
+++ state/ingress.go
@@ -6,110 +6,180 @@ import (
 	"github.com/rs/zerolog/log"
 	v1Core "k8s.io/api/core/v1"
 	v1Net "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/labels"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"net"
-	"strings"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 	"sync"
+
+	_ "k8s.io/apimachinery/pkg/fields"               // Required for Watching
+	_ "k8s.io/apimachinery/pkg/types"                // Required for Watching
+	_ "sigs.k8s.io/controller-runtime/pkg/builder"   // Required for Watching
+	_ "sigs.k8s.io/controller-runtime/pkg/handler"   // Required for Watching
+	_ "sigs.k8s.io/controller-runtime/pkg/predicate" // Required for Watching
+	_ "sigs.k8s.io/controller-runtime/pkg/reconcile" // Required for Watching
+	_ "sigs.k8s.io/controller-runtime/pkg/source"    // Required for Watching
 )
 
-type IngressStateManager struct {
-	k8sClients       *kubernetesClients
-	ingressStateChan chan IngressState
-	ingressClassName string
-	hostIp           net.IP
+// IngressReconciler holds the main logic of the ingress controller regarding state updating
+type IngressReconciler struct {
+	k8sClients                *kubernetesClients
+	ingressStateLock          sync.RWMutex
+	ingressState              map[types.NamespacedName]*v1Net.Ingress
+	ingressProcessedStateChan chan IngressState
+	ingressClassName          string
+	hostIp                    net.IP
+	manager                   ctrl.Manager
 }
 
-type BackendPath struct {
-	PathType    *v1Net.PathType
-	Path        string
-	Namespace   string
-	ServiceName string
-	ServicePort int32
-}
-
-type TlsCert struct {
-	Cert []byte
-	Key  []byte
-}
-
-type DomainConfig struct {
-	BackendPaths []*BackendPath
-	TlsCert      *TlsCert
-}
-
-type IngressState map[string]*DomainConfig
-
-// getOrAddEmpty returns the map value for the given key. If the key does not exist in the map an empty entry is created and returned.
-func (state IngressState) getOrAddEmpty(key string) *DomainConfig {
-	val, ok := state[key]
-	if ok {
-		return val
-	}
-	val = &DomainConfig{
-		BackendPaths: make([]*BackendPath, 0),
-	}
-	state[key] = val
-	return val
-}
-
-// New creates a new Kubernetes IngressInformer state. The ctx can be used to cancel the listening to updates from the Kubernetes API.
+// New creates a new Kubernetes Ingress reconsiler and registers it with the manager.
 // The hostIp is an optional argument. If and only if it is set the ingress status is updated.
-func New(ctx context.Context, client kubernetes.Interface, ingressClassName string, hostIp net.IP) (*IngressStateManager, error) {
-	k8sClients, err := newKubernetesClients(ctx, client)
-	stateManager := &IngressStateManager{
-		ingressClassName: ingressClassName,
-		ingressStateChan: make(chan IngressState),
-		hostIp:           hostIp,
-		k8sClients:       k8sClients,
-	}
+func New(mgr ctrl.Manager, ingressClassName string, hostIp net.IP) (*IngressReconciler, error) {
+	k8sClients, err := kubernetes.NewForConfig(mgr.GetConfig())
 	if err != nil {
-		return nil, fmt.Errorf("failed to setup kubernetes clients")
+		return nil, fmt.Errorf("error constructing k8s clients from manager config: %v", err)
+	}
+	r := &IngressReconciler{
+		ingressClassName:          ingressClassName,
+		ingressState:              make(map[types.NamespacedName]*v1Net.Ingress),
+		ingressProcessedStateChan: make(chan IngressState),
+		hostIp:                    hostIp,
+		k8sClients:                newKubernetesClients(k8sClients),
+		manager:                   mgr,
+	}
+	return r, ctrl.NewControllerManagedBy(mgr).
+		For(&v1Net.Ingress{}).
+		Watches(&source.Kind{Type: &v1Core.Secret{}},
+			handler.EnqueueRequestsFromMapFunc(r.findIngressForSecret),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{})).
+		Watches(&source.Kind{Type: &v1Core.Service{}},
+			handler.EnqueueRequestsFromMapFunc(r.findIngressForService),
+			builder.WithPredicates(predicate.ResourceVersionChangedPredicate{})).
+		Complete(r)
+}
+
+// GetStateChan returns a read-only channel that carries the current state
+func (r *IngressReconciler) GetStateChan() <-chan IngressState {
+	return r.ingressProcessedStateChan
+}
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
+func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log.Debug().Msgf("reconciling ingress: %v", req)
+	ingress, err := r.k8sClients.client.NetworkingV1().Ingresses(req.Namespace).Get(ctx, req.Name, metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return ctrl.Result{Requeue: true}, fmt.Errorf("error fetching ingress state: %v", err)
+	}
+	r.ingressStateLock.Lock()
+	defer r.ingressStateLock.Unlock()
+	if apierrors.IsNotFound(err) {
+		log.Debug().Msgf("reconcile deleting ingress reference: %v", req)
+		delete(r.ingressState, req.NamespacedName)
 	}
 
-	go func() {
-		for {
-			select {
-			case <-stateManager.k8sClients.addUpdDelChan:
-				stateManager.refetchState(ctx)
-			case <-ctx.Done():
-				return
+	if ingress != nil && ((ingress.Spec.IngressClassName != nil && *ingress.Spec.IngressClassName != r.ingressClassName) ||
+		(ingress.Spec.IngressClassName == nil && ingress.Annotations["kubernetes.io/ingress.class"] != r.ingressClassName)) {
+		log.Debug().Msgf("reconcile ignoring ingress due to class-name: %v", req)
+		return ctrl.Result{}, nil
+	}
+	log.Debug().Msgf("reconcile processing ingress: %v", req)
+	r.ingressState[req.NamespacedName] = ingress.DeepCopy()
+	processedState, updates := r.processState()
+	r.ingressProcessedStateChan <- processedState
+	errors := r.updateStatus(ctx, updates)
+	for _, err := range errors {
+		// errors are missing values in the referenced services/secrets, no need to retry as we watch those resources
+		log.Error().Err(err).Msg("failed to update ingress status")
+	}
+	return ctrl.Result{}, nil
+}
+
+// Start sets up the informers and the controller with the Manager, blocks till the context is cancelled or an error occurs.
+func (r *IngressReconciler) Start(ctx context.Context) error {
+	if err := r.k8sClients.startInformers(ctx); err != nil {
+		return fmt.Errorf("failed to start kubernetes informers: %v", err)
+	}
+	r.k8sClients.waitForSync(ctx)
+
+	return r.manager.Start(ctx)
+}
+
+func (r *IngressReconciler) findIngressForSecret(secret client.Object) []reconcile.Request {
+	log.Debug().Msgf("watch triggered from secret %s in namespace %s", secret.GetName(), secret.GetNamespace())
+	r.ingressStateLock.RLock()
+	defer r.ingressStateLock.RUnlock()
+	requests := make([]reconcile.Request, 0)
+	for _, el := range r.ingressState {
+		if el.Namespace != secret.GetNamespace() {
+			continue
+		}
+		if referencesSecret(el, secret) {
+			log.Debug().Msgf("reconcile queued due to secret update for ingress %s in namespace %s", el.Name, el.Namespace)
+			requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{
+				Name:      el.Name,
+				Namespace: el.Namespace,
+			}})
+		}
+	}
+	return requests
+}
+
+// referencesSecret returns whether the ingress references the given secret
+func referencesSecret(el *v1Net.Ingress, secret client.Object) bool {
+	for _, tls := range el.Spec.TLS {
+		if tls.SecretName == secret.GetName() {
+			return true
+		}
+	}
+	return false
+}
+
+func (r *IngressReconciler) findIngressForService(service client.Object) []reconcile.Request {
+	log.Debug().Msgf("watch triggered from service %s in namespace %s", service.GetName(), service.GetNamespace())
+	r.ingressStateLock.RLock()
+	defer r.ingressStateLock.RUnlock()
+	requests := make([]reconcile.Request, 0)
+	for _, el := range r.ingressState {
+		if el.Namespace != service.GetNamespace() {
+			continue
+		}
+		if referencesService(el, service) {
+			log.Debug().Msgf("reconcile queued due to service update for ingress %s in namespace %s", el.Name, el.Namespace)
+			requests = append(requests, reconcile.Request{NamespacedName: types.NamespacedName{
+				Name:      el.Name,
+				Namespace: el.Namespace,
+			}})
+		}
+	}
+	return requests
+}
+
+// referencedService returns whether the ingress references the given service
+func referencesService(el *v1Net.Ingress, service client.Object) bool {
+	for _, rule := range el.Spec.Rules {
+		for _, path := range rule.HTTP.Paths {
+			if path.Backend.Service.Name == service.GetName() {
+				return true
 			}
 		}
-	}()
-	return stateManager, k8sClients.startInformers(ctx)
-}
-
-// GetStateChan returns a channel where state updates are delivered. This is the main method used to fetch the current status.
-func (stateManager *IngressStateManager) GetStateChan() <-chan IngressState {
-	return stateManager.ingressStateChan
-}
-
-// getIngresses fetches the list of ingresses with the relevant ingress class from the kubernetes api
-func (stateManager *IngressStateManager) getIngresses() ([]*v1Net.Ingress, error) {
-	ingresses, err := stateManager.k8sClients.IngressInformer.Lister().List(labels.Everything())
-	if err != nil {
-		return nil, fmt.Errorf("error fetching ingress list: %v", err)
 	}
-	//filter the ingress class
-	n := 0
-	for _, el := range ingresses {
-		if (el.Spec.IngressClassName != nil && *el.Spec.IngressClassName == stateManager.ingressClassName) ||
-			(el.Spec.IngressClassName == nil && el.Annotations["kubernetes.io/ingress.class"] == stateManager.ingressClassName) {
-			ingresses[n] = el
-			n++
-		}
-	}
-	return ingresses[:n], nil
+	return false
 }
 
 // CleanIngressStatus is supposed to be called during shutdown and removes all ingress status entries set by this instance.
 // The internal state channel is not updated.
-func (stateManager *IngressStateManager) CleanIngressStatus(ctx context.Context) []error {
-	ingresses, err := stateManager.getIngresses()
-	if err != nil {
-		return []error{err}
-	}
+func (r *IngressReconciler) CleanIngressStatus(ctx context.Context) []error {
 	errors := make([]error, 0)
 	errChan := make(chan error)
 	defer close(errChan) // to stop the error collection goroutine
@@ -120,10 +190,12 @@ func (stateManager *IngressStateManager) CleanIngressStatus(ctx context.Context)
 	}()
 
 	var wg sync.WaitGroup
-	wg.Add(len(ingresses))
-	for _, el := range ingresses {
+	r.ingressStateLock.Lock()
+	defer r.ingressStateLock.Unlock()
+	wg.Add(len(r.ingressState))
+	for _, el := range r.ingressState {
 		go func(ingress *v1Net.Ingress) {
-			err := stateManager.k8sClients.cleanIngressStatus(ctx, ingress, stateManager.hostIp)
+			err := r.k8sClients.cleanIngressStatus(ctx, ingress, r.hostIp)
 			if err != nil {
 				errChan <- fmt.Errorf("could not clean ingress status: %v", err)
 			}
@@ -132,158 +204,5 @@ func (stateManager *IngressStateManager) CleanIngressStatus(ctx context.Context)
 	}
 	wg.Wait()
 
-	return errors
-}
-
-// refetchState is used to collect a new state from the Kubernetes API from scratch.
-func (stateManager *IngressStateManager) refetchState(ctx context.Context) {
-	ingresses, err := stateManager.getIngresses()
-	if err != nil {
-		log.Error().Err(err).Msg("error listening ingresses")
-		return
-	}
-
-	ingressState := make(IngressState)
-	updates := make([]*ingressStatusUpdate, 0)
-	for _, ingress := range ingresses {
-		errors := stateManager.collectBackendPaths(ingress, ingressState)
-		errors = append(errors, stateManager.collectTlsSecrets(ingress, ingressState)...)
-		log.Debug().Msgf("ingress errors: %v", errors)
-		if stateManager.hostIp != nil {
-			updates = append(updates, &ingressStatusUpdate{
-				Ingress: ingress.DeepCopy(),
-				Status:  statusFromErrors(errors, stateManager.hostIp),
-			})
-		}
-	}
-	stateManager.ingressStateChan <- ingressState
-	if stateManager.hostIp != nil {
-		var wg sync.WaitGroup
-		wg.Add(len(updates))
-		for _, el := range updates {
-			go func(update *ingressStatusUpdate) {
-				err := stateManager.k8sClients.updateIngressStatus(ctx, update.Ingress, update.Status)
-				if err != nil {
-					log.Error().Err(err).Msg("failed to update ingress status")
-				}
-				wg.Done()
-			}(el)
-		}
-		// make this blocking to make sure that we not start a new fetch before the status is updated
-		wg.Wait()
-	}
-}
-
-// statusFromErrors builds an ingress status from the given error list
-func statusFromErrors(errors []error, hostIp net.IP) *v1Net.IngressLoadBalancerIngress {
-	var errMsg *string
-	if len(errors) > 0 {
-		var sb strings.Builder
-		for i, err := range errors {
-			sb.WriteString(err.Error())
-			if i < len(errors)-1 {
-				sb.WriteString(";")
-			}
-		}
-		errMsgCollected := sb.String()
-		errMsg = &errMsgCollected
-	}
-	return &v1Net.IngressLoadBalancerIngress{
-		IP: hostIp.String(),
-		Ports: []v1Net.IngressPortStatus{
-			{Port: 80,
-				Protocol: "TCP",
-				Error:    errMsg,
-			},
-			{Port: 443,
-				Protocol: "TCP",
-				Error:    errMsg,
-			},
-		},
-	}
-}
-
-// collectsBackendPaths collects the relevant backend path information and adds them to the ingress state. It also collects port numbers from referenced services.
-func (stateManager *IngressStateManager) collectBackendPaths(ingress *v1Net.Ingress, result IngressState) []error {
-	errors := make([]error, 0)
-	for _, rule := range ingress.Spec.Rules {
-		if rule.HTTP == nil {
-			continue
-		}
-		domainConfig := result.getOrAddEmpty(rule.Host)
-		backendPaths := make([]*BackendPath, 0)
-		for _, path := range rule.HTTP.Paths {
-			backendPath := &BackendPath{
-				PathType:    path.PathType,
-				Path:        path.Path,
-				Namespace:   ingress.Namespace,
-				ServiceName: path.Backend.Service.Name,
-				ServicePort: path.Backend.Service.Port.Number,
-			}
-			err := stateManager.updatePortFromService(backendPath, path.Backend.Service.Port.Name)
-			if err != nil {
-				log.Warn().Err(err).Msg("could not determine service port")
-				errors = append(errors, fmt.Errorf("ngergs.de/ServicePortNotFound: %s for backend service %s", path.Backend.Service.Port.Name, path.Backend.Service.Name))
-				continue
-			} else {
-				backendPaths = append(backendPaths, backendPath)
-			}
-		}
-		domainConfig.BackendPaths = append(domainConfig.BackendPaths, backendPaths...)
-	}
-	return errors
-}
-
-// updatePortFromService uses the Kubernetes API to fetch the ServiceInformer status for the service referenced in the ingress config.
-// If this has finished without error the config.ServicePort property is guaranteed to be set according to the current service spec.
-func (stateManager *IngressStateManager) updatePortFromService(config *BackendPath, servicePortName string) error {
-	if config.ServicePort == 0 && servicePortName == "" {
-		return fmt.Errorf("invalid config for path %s. Backend service does contain neither port name nor port number", config.Path)
-	}
-	svc, err := stateManager.k8sClients.ServiceInformer.Lister().Services(config.Namespace).Get(config.ServiceName)
-	if err != nil {
-		return err
-	}
-
-	// matching number takes precedence
-	for _, svcPort := range svc.Spec.Ports {
-		if svcPort.Port == config.ServicePort {
-			return nil
-		}
-	}
-	for _, svcPort := range svc.Spec.Ports {
-		if svcPort.Name == servicePortName {
-			config.ServicePort = svcPort.Port
-			return nil
-		}
-	}
-	return fmt.Errorf("port name %s specified but not found in service %s in namespace %s", servicePortName, config.ServiceName, config.Namespace)
-}
-
-// collectTlsSecrets fetches for all secrets that are referenced in the ingresses the relevant kubernetes.io/tls secrets from the Kubernetes API and adds them to the ingressState
-func (stateManager *IngressStateManager) collectTlsSecrets(ingress *v1Net.Ingress, result IngressState) []error {
-	errors := make([]error, 0)
-	for _, rule := range ingress.Spec.TLS {
-		secret, err := stateManager.k8sClients.SecretInformer.Lister().Secrets(ingress.Namespace).Get(rule.SecretName)
-		if err != nil {
-			log.Warn().Err(err).Msgf("error getting ingress TLS certificate secret %s in namespace %s",
-				rule.SecretName, ingress.Namespace)
-			errors = append(errors, fmt.Errorf("ngergs.de/TlsCertMissing: referenced secret %s", rule.SecretName))
-			continue
-		}
-		if secret.Type != v1Core.SecretTypeTLS {
-			log.Warn().Msgf("SecretInformer type mismatch, required kubernetes.io/tls, but found %s for secret %s in namespace %s",
-				secret.Type, secret.Name, secret.Namespace)
-			errors = append(errors, fmt.Errorf("ngergs.de/TlsCertWrongType: has to be kubernetees.io/tls"))
-			continue
-		}
-		for _, host := range rule.Hosts {
-			domainConfig := result.getOrAddEmpty(host)
-			domainConfig.TlsCert = &TlsCert{
-				Cert: secret.Data["tls.crt"],
-				Key:  secret.Data["tls.key"],
-			}
-		}
-	}
 	return errors
 }
